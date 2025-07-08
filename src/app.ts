536239cb81370f4a6fa179cc4f90946b5ec4e55b
Gabriel Peña <gapena@ri.gob.do>
2025-07-08 21:39:12 +0200

    Segunda leccion

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log("Hola mundo")
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 3000 });
+
+wss.on('connection', function connection(ws) {
+    console.log("Client connected")
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+  ws.send('Hola desde el servidor');
+
+  setInterval(() => {
+    ws.send("Hola de nuevo")
+  }, 2000);
+});
+
+console.log('Server running on port http://localhost:3000')
