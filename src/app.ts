1335670c9b72fc3c07ea0eb61b99d8174990e284
Gabriel Peña <gapena@ri.gob.do>
2025-07-09 14:39:02 +0200

    Continuacion

--- src/app.ts
+++ src/app.ts
@@ -3,18 +3,22 @@ import { WebSocketServer } from 'ws';
 const wss = new WebSocketServer({ port: 3000 });
 
 wss.on('connection', function connection(ws) {
+
+    console.log(ws);
+
     console.log("Client connected")
-  ws.on('error', console.error);
 
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
-  });
+    ws.on('error', console.error);
 
-  ws.send('Hola desde el servidor');
+    ws.on('message', function message(data) {
+        console.log('received: %s', data);
+    });
 
-  setInterval(() => {
-    ws.send("Hola de nuevo")
-  }, 2000);
+    ws.send('Hola desde el servidor');
+
+    ws.on('close', () => {
+        console.log('Client disconnected')
+    })
 });
 
-console.log('Server running on port http://localhost:3000')
+console.log('Server running on port ws://localhost:3000')
