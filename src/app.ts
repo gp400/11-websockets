d8c869e4a1f611e9e30e395503e0b3c6167f5d54
Gabriel Peña <gapena@ri.gob.do>
2025-07-11 20:12:20 +0200

    Multiples

--- src/app.ts
+++ src/app.ts
@@ -11,12 +11,26 @@ wss.on('connection', function connection(ws) {
     ws.on('error', console.error);
 
     ws.on('message', function message(data) {
-        console.log('Desde el cliente', data);
-        const payload = {
+        
+        const payload = JSON.stringify({
             type: 'custom-message',
             payload: data.toString()
-        }
-        ws.send(JSON.stringify(payload));
+        })
+        // ws.send(JSON.stringify(payload));
+
+        // //Todos - incluyente
+        // wss.clients.forEach(function each(client) {
+        //     if (client.readyState === WebSocket.OPEN) {
+        //         client.send(payload, { binary: false });
+        //     }
+        // });
+
+        //Todos excluyente
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(payload, { binary: false });
+            }
+        });
     });
 
     // ws.send('Hola desde el servidor');
